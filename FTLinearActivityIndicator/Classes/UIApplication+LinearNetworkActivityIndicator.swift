4f429e09a21d42d97ec78a0d03952bce7eb95c28
Ortwin Gentz <gentz@futuretap.com>
2018-01-04 14:45:19 +0100

    Decrease network activity indicator height to 4 pt

--- FTLinearActivityIndicator/Classes/UIApplication+LinearNetworkActivityIndicator.swift
+++ FTLinearActivityIndicator/Classes/UIApplication+LinearNetworkActivityIndicator.swift
@@ -59,7 +59,7 @@ extension UIApplication {
 				indicatorWindow = UIWindow(frame: statusBarFrame)
 				indicatorWindow?.windowLevel = UIWindowLevelStatusBar + 1
 				
-				let indicator = FTLinearActivityIndicator(frame: CGRect(x: indicatorWindow!.frame.width - 74, y: 5, width: 44, height: 6))
+				let indicator = FTLinearActivityIndicator(frame: CGRect(x: indicatorWindow!.frame.width - 74, y: 6, width: 44, height: 4))
 				indicator.tintColor = statusBarStyle == .default ? UIColor.black : UIColor.white
 				indicator.hidesWhenStopped = false
 				indicator.startAnimating()
