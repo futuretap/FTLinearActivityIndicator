2e049ec2f5012ea364df3f8d2e17641165db9dbd
Darren Ehlers <me@darrenehlers.com>
2019-11-20 10:48:06 -0600

    Reverted source move to Sources subdirectory

--- Package.swift
+++ Package.swift
@@ -23,7 +23,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "FTLinearActivityIndicator",
-            dependencies: [])
+            dependencies: [],
+            path: "FTLinearActivityIndicator")
     ],
     swiftLanguageVersions: [.v5]
 )
