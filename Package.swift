8a47161071e29291cf4dcee0829a1205a4a5953a
Ortwin Gentz <gentz@futuretap.com>
2023-07-18 23:36:42 +0200

    Use stringified version in Package.swift
    should prevent deprecation warning

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "FTLinearActivityIndicator",
     platforms: [
-        .iOS(.v9)
+        .iOS("9.0")
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
